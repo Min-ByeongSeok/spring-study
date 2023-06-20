49e303e0d5974499b13fc76e8a06ef683a2a5f24
Min-ByeongSeok <mbs8801@gmail.com>
2023-06-20 16:01:58 +0900

    feat : zerobase practice

--- convpay/src/main/java/com/zerobase/convpay/UserClient.java
+++ convpay/src/main/java/com/zerobase/convpay/UserClient.java
@@ -1,6 +1,6 @@
 package com.zerobase.convpay;
 
-import com.zerobase.convpay.config.ApplicationConfig;
+//import com.zerobase.convpay.config.ApplicationConfig;
 import com.zerobase.convpay.dto.PayCancelRequest;
 import com.zerobase.convpay.dto.PayCancelResponse;
 import com.zerobase.convpay.dto.PayRequest;
@@ -10,12 +10,14 @@ import com.zerobase.convpay.type.ConvenienceType;
 import com.zerobase.convpay.type.PayMethodType;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserClient {
     public static void main(String[] args) {
 
         ApplicationContext applicationContext
-                = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+                = new ClassPathXmlApplicationContext("spring-config.xml");
+//                = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         ConveniencePayService conveniencePayService
                 = applicationContext
                 .getBean("conveniencePayService", ConveniencePayService.class);
