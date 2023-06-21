8fb493d3addb402cfd44f1d3d3b9a9d4e080a3b9
Min-ByeongSeok <mbs8801@gmail.com>
2023-06-22 00:25:20 +0900

    feat : zerobase Account practice

--- Account/src/main/java/com/example/account/controller/AccountController.java
+++ Account/src/main/java/com/example/account/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.example.account.controller;
 import com.example.account.domain.Account;
 import com.example.account.dto.AccountDto;
 import com.example.account.dto.CreateAccount;
+import com.example.account.dto.DeleteAccount;
 import com.example.account.service.AccountService;
 import com.example.account.service.RedisTestService;
 import lombok.RequiredArgsConstructor;
@@ -17,21 +18,30 @@ public class AccountController {
     private final RedisTestService redisTestService;
 
     @PostMapping("/account")
-    public CreateAccount.Response createAccount(@RequestBody @Valid CreateAccount.Request request){
+    public CreateAccount.Response createAccount(@RequestBody @Valid CreateAccount.Request request) {
         return CreateAccount.Response.from(
                 accountService
                         .createAccount(
-                        request.getUserId(),
-                        request.getInitialBalance()));
+                                request.getUserId(),
+                                request.getInitialBalance()));
+    }
+
+    @DeleteMapping("/account")
+    public DeleteAccount.Response deleteAccount(@RequestBody @Valid DeleteAccount.Request request) {
+        return DeleteAccount.Response.from(
+                accountService
+                        .deleteAccount(
+                                request.getUserId(),
+                                request.getAccountNumber()));
     }
 
     @GetMapping("/get-lock")
-    public String getLock(){
+    public String getLock() {
         return redisTestService.getLock();
     }
 
     @GetMapping("/account/{id}")
-    public Account getAccount(@PathVariable Long id){
+    public Account getAccount(@PathVariable Long id) {
         return accountService.getAccount(id);
     }
 }
