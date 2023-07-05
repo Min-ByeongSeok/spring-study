90da22311e42bdd5850f501e7cb14ac8b7c1cc27
Min-ByeongSeok <mbs8801@gmail.com>
2023-07-05 11:23:39 +0900

    delete : useless project

--- dividend/src/main/java/zerobase/dividend/scraper/Scraper.java
+++
@@ -1,9 +0,0 @@
-package zerobase.dividend.scraper;
-
-import zerobase.dividend.model.Company;
-import zerobase.dividend.model.ScrapedResult;
-
-public interface Scraper {
-    Company scrapCompanyByTicker(String ticker);
-    ScrapedResult scrap(Company company);
-}
--- dividend/src/main/java/zerobase/dividend/web/AuthController.java
+++
@@ -1,4 +0,0 @@
-package zerobase.dividend.web;
-
-public class AuthController {
-}
