0d6bc288decc890f805f6b26bd2783039ef5b0b8
Min-ByeongSeok <mbs8801@gmail.com>
2023-08-04 11:22:53 +0900

    feat zerobase mission project

--- zerobase-mission/src/main/java/zerobase/mission/controller/RegisterController.java
+++ zerobase-mission/src/main/java/zerobase/mission/controller/RegisterController.java
@@ -1,10 +1,6 @@
 package zerobase.mission.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +8,14 @@ import zerobase.mission.domain.Store;
 import zerobase.mission.dto.RegisterStoreDto;
 import zerobase.mission.dto.StoreDto;
 import zerobase.mission.service.RegisterStoreService;
-import zerobase.mission.service.ReservationService;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class RegisterController {
 
     private final RegisterStoreService registerStoreService;
-    private final ReservationService reservationService;
 
     @PostMapping("/register/store")
     public RegisterStoreDto.Response registerStore(@RequestBody @Valid RegisterStoreDto.Request request) {
@@ -38,11 +31,4 @@ public class RegisterController {
         return RegisterStoreDto.Response.fromDto(storeDto);
     }
 
-    @GetMapping("/storelist")
-    public ResponseEntity<?> getStoreList(final Pageable pageable) {
-//    public List<StoreListDto.Response> getStoreList(@RequestBody @Valid StoreListDto.Request request) {
-        Page<Store> allStore = reservationService.getAllStore(pageable);
-
-        return ResponseEntity.ok(allStore);
-    }
 }
